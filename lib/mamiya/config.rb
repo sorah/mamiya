9ecab605426f2bfc0f5dcac30d32a4a5fbac3b1e
Shota Fukumori (sora_h) <sorah@cookpad.com>
2014-03-07 16:19:56 +0900

    Add Mamiya::Config for configuration
    - Using Mamiya::DSL for DSL feature

---
+++ lib/mamiya/config.rb
@@ -0,0 +1,50 @@
+require 'mamiya/dsl'
+
+module Mamiya
+  class Config < DSL
+    add_hook :before_build
+    add_hook :prepare_build
+    add_hook :build
+    add_hook :after_build
+
+    add_hook :before_distribute
+    add_hook :after_distribute
+
+    add_hook :before_prepare
+    add_hook :prepare
+    add_hook :after_prepare
+
+    add_hook :before_finalize
+    add_hook :finalize
+    add_hook :after_finalize
+
+    add_hook :before_rollback
+    add_hook :rollback
+    add_hook :after_rollback
+
+    add_hook :package_name, chain: true
+
+    set_default :application, nil
+    set_default :repository, nil
+    set_default :ref, nil
+
+    set_default :discover_servers, true
+    set_default :on_client_failure, :error # error, warn, ignore
+
+    set_default :package_under, nil
+    set_default :exclude_from_package, nil
+
+    set_default :package_to, nil
+    set_default :deploy_to, nil
+    set_default :prepare_to, nil
+
+    def servers
+    end
+
+    def servers_use
+    end
+
+    def storage(name, options={})
+    end
+  end
+end
