6671f75a78b0d57214142ae3bb90348d76124778
Shota Fukumori (sora_h) <sorah@cookpad.com>
2014-03-11 16:17:20 +0900

    Implement Mamiya::Package

--- lib/mamiya/package.rb
+++ lib/mamiya/package.rb
@@ -1,10 +1,104 @@
+require 'pathname'
+require 'digest/sha2'
+
 module Mamiya
   class Package
     class NotExists < Exception; end
+    class InternalError < Exception; end
+    TARBALL_EXT = '.tar.bz2'
 
     def initialize(path)
+      @path = Pathname.new(path)
+      @meta_loaded_from_file = false
+      @loaded_meta = nil
+      meta # load
     end
 
-    attr_accessor :meta
+    attr_reader :path
+    attr_writer :meta
+
+    def name
+      meta['name'] || @path.basename(TARBALL_EXT).to_s
+    end
+
+    def meta_path
+      @path.sub_ext('').sub_ext('.json')
+    end
+
+    def meta
+      if !@meta_loaded_from_file && meta_path.exist?
+        @meta_loaded_from_file = true
+        loaded_meta = load_meta()
+        if @loaded_meta == @meta
+          @loaded_meta = loaded_meta
+          @meta = load_meta()
+        end
+      end
+      @meta ||= {}
+    end
+
+    def checksum
+      return nil unless exist?
+      Digest::SHA2.file(path).hexdigest
+    end
+
+    def valid?
+      raise NotExists, 'package not exist' unless exist?
+      raise NotExists, 'meta not exist' unless meta_path.exist?
+      !meta['checksum'] || checksum == meta['checksum']
+    end
+
+    def exists?
+      path.exist?
+    end
+    alias exist? exists?
+
+    def build!(build_dir, exclude_from_package: [], dereference_symlinks: false, package_under: nil)
+      exclude_from_package.push('.svn', '.git').uniq!
+
+      build_dir = Pathname.new(build_dir)
+      build_dir += package_under if package_under
+
+      meta['name'] = self.name
+      File.write build_dir.join('.mamiya.meta.json'), self.meta.to_json
+
+      Dir.chdir(build_dir) do
+        excludes = exclude_from_package.flat_map { |exclude| ['--exclude', exclude] }
+        dereference = dereference_symlinks ? ['-h'] : []
+
+        cmd = ["tar", "cjf", self.path.to_s,
+               *dereference,
+               *excludes,
+               "."]
+
+        result = system(*cmd)
+        raise InternalError, "failed to run: #{cmd.inspect}" unless result
+      end
+
+      checksum = self.checksum()
+      raise InternalError, 'checksum should not be nil after package built' unless checksum
+      meta['checksum'] = checksum
+
+      File.write meta_path, self.meta.to_json
+      nil
+    end
+
+    def extract_onto!(path)
+      raise NotExists unless exist?
+      Dir.mkdir(path) unless File.directory?(path)
+
+      Dir.chdir(path) do
+        cmd = ["tar", "xf", @path.to_s]
+        result = system(*cmd)
+        raise InternalError, "Failed to run: #{cmd.inspect}" unless result
+      end
+      nil
+    end
+
+    private
+
+    def load_meta
+      meta_path.exist? && JSON.parse(meta_path.read)
+    end
   end
 end
