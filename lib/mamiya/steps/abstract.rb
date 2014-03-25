415e9ce855afaecad91bcb246f8ac77bc7890a62
Shota Fukumori (sora_h) <sorah@cookpad.com>
2014-03-25 16:25:21 +0900

    Steps::Abstract: Logger support
    - Automatically assigns its class name to progname

--- lib/mamiya/steps/abstract.rb
+++ lib/mamiya/steps/abstract.rb
@@ -1,14 +1,15 @@
 require 'mamiya/config'
 require 'mamiya/script'
+require 'mamiya/logger'
 
 module Mamiya
   module Steps
     class Abstract
-      def initialize(script: Mamiya::Script.new, config: Mamiya::Config.new, **options)
-        @script, @config, @options = script, config, options
+      def initialize(script: Mamiya::Script.new, config: Mamiya::Config.new, logger: Mamiya::Logger.new, **options)
+        @script, @config, @logger, @options = script, config, logger[self.class.name], options
       end
 
-      attr_reader :script, :config, :options
+      attr_reader :script, :config, :options, :logger
 
       def run!
       end
