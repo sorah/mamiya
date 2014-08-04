c52a5f157ad8db95258a67f6622b12a4eeca6274
Shota Fukumori (sora_h) <sorah@cookpad.com>
2014-08-04 14:41:16 +0900

    [bug] don't remove when raising AlreadyFetched

--- lib/mamiya/storages/s3.rb
+++ lib/mamiya/storages/s3.rb
@@ -60,11 +60,16 @@ module Mamiya
         end
 
         return Mamiya::Package.new(package_path)
+
+      rescue AlreadyFetched, NotFound => e
+        raise e
+
       rescue Aws::S3::Errors::NoSuchKey
         File.unlink package_path if package_path && File.exists?(package_path)
         File.unlink meta_path if meta_path && File.exists?(meta_path)
 
         raise NotFound
+
       rescue Exception => e
         File.unlink package_path if package_path && File.exists?(package_path)
         File.unlink meta_path if meta_path && File.exists?(meta_path)
--- spec/storages/s3_spec.rb
+++ spec/storages/s3_spec.rb
@@ -164,6 +164,14 @@ describe Mamiya::Storages::S3 do
           fetch
         }.to raise_error(Mamiya::Storages::Abstract::AlreadyFetched)
       end
+
+      it "doesn't remove anything" do
+        begin
+          fetch
+        rescue Mamiya::Storages::Abstract::AlreadyFetched; end
+
+        expect(File.exist?(metafile)).to be_true
+      end
     end
 
     context "when tarball already exists" do
@@ -175,6 +183,14 @@ describe Mamiya::Storages::S3 do
         expect {
           fetch
         }.to raise_error(Mamiya::Storages::Abstract::AlreadyFetched)
+      end
+
+      it "doesn't remove anything" do
+        begin
+          fetch
+        rescue Mamiya::Storages::Abstract::AlreadyFetched; end
+
+        expect(File.exist?(tarball)).to be_true
       end
     end
 
