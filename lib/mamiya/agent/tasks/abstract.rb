2ac7b4fc15f6d96f95c37067320210825a77306f
Shota Fukumori (sora_h) <sorah@cookpad.com>
2014-08-25 10:52:24 +0900

    Add #task_id and use as logger title

--- lib/mamiya/agent/tasks/abstract.rb
+++ lib/mamiya/agent/tasks/abstract.rb
@@ -7,11 +7,11 @@ module Mamiya
       class Abstract
         def initialize(task_queue, task, agent: nil, logger: Mamiya::Logger.new, raise_error: false)
           @agent = agent
-          @logger = logger
           @task_queue = task_queue
           @task = task.merge('task' => self.class.identifier)
           @error = nil
           @raise_error = raise_error
+          @logger = logger["#{self.class.identifier}:#{self.task_id}"]
         end
 
         def self.identifier
@@ -22,6 +22,10 @@ module Mamiya
 
         def raise_error?
           !!@raise_error
+        end
+
+        def task_id
+          task['id'] || "0x#{self.__id__.to_s(16)}"
         end
 
         def execute
