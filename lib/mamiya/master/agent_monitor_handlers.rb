36ee8f2e8c6cf590e441bc62f5bbdcb186e12f5d
Shota Fukumori (sora_h) <sorah@cookpad.com>
2014-08-26 17:14:50 +0900

    Remove fetcher support from agent_monitor

--- lib/mamiya/master/agent_monitor_handlers.rb
+++ lib/mamiya/master/agent_monitor_handlers.rb
@@ -68,53 +68,6 @@ module Mamiya
         packages = status['packages'][payload['application']]
         packages.delete(payload['package']) if packages
       end
-
-
-
-      def fetch_result__ack(status, payload, event)
-        status['fetcher'] ||= {}
-        status['fetcher']['pending'] = payload['pending']
-
-        status['fetcher']['pending_jobs'] ||= []
-        status['fetcher']['pending_jobs'] << [payload['application'], payload['package']]
-      end
-
-      def fetch_result__start(status, payload, event)
-        status['fetcher'] ||= {}
-        status['fetcher']['fetching'] = [payload['application'], payload['package']]
-
-        logger.debug "#{status['name']} started to fetch #{payload['application']}/#{payload['package']}"
-
-        status['fetcher']['pending_jobs'] ||= []
-        status['fetcher']['pending_jobs'].delete [payload['application'], payload['package']]
-      end
-
-      def fetch_result__error(status, payload, event)
-        status['fetcher'] ||= {}
-
-        logger.error "#{status['name']} failed to fetch #{payload['application']}/#{payload['package']}: #{payload['error']}"
-
-        if status['fetcher']['fetching'] == [payload['application'], payload['package']]
-          status['fetcher']['fetching'] = nil
-        end
-      end
-
-      def fetch_result__success(status, payload, event)
-        status['fetcher'] ||= {}
-
-        logger.info "#{status['name']} fetched #{payload['application']}/#{payload['package']}"
-
-        if status['fetcher']['fetching'] == [payload['application'], payload['package']]
-          status['fetcher']['fetching'] = nil
-        end
-
-        status['packages'] ||= {}
-        status['packages'][payload['application']] ||= []
-        status['packages'][payload['application']] << payload['package']
-      end
-
-      # TODO: XXX: deprecated
-      alias fetch_result__remove pkg__remove
     end
   end
 end
