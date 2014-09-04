b17fd44eadf4b034917105821d4f48cb8d686970
Shota Fukumori (sora_h) <sorah@cookpad.com>
2014-09-04 10:35:50 +0900

    Add Mamiya::Steps::Prepare

---
+++ spec/steps/prepare_spec.rb
@@ -0,0 +1,90 @@
+require 'spec_helper'
+require 'tmpdir'
+require 'pathname'
+require 'fileutils'
+require 'json'
+
+require 'mamiya/script'
+
+require 'mamiya/package'
+require 'mamiya/steps/prepare'
+
+describe Mamiya::Steps::Prepare do
+  let!(:tmpdir) { Dir.mktmpdir("mamiya-steps-prepare-spec") }
+  after { FileUtils.remove_entry_secure tmpdir }
+
+  let(:target_dir) { Pathname.new(tmpdir).join('release').tap(&:mkdir) }
+
+  let(:target_meta) do
+    {
+      script: 'script.rb',
+    }
+  end
+
+  let(:script) do
+    double('script',
+      before_prepare: proc {},
+      prepare: proc {},
+      after_prepare: proc {},
+    )
+  end
+
+  let(:config) do
+    double('config')
+  end
+
+  let(:options) do
+    {target: target_dir}
+  end
+
+  subject(:step) { described_class.new(script: nil, config: config, **options) }
+
+  before do
+    File.write target_dir.join('.mamiya.meta.json'), target_meta.to_json
+    allow(Mamiya::Script).to receive(:new).and_return(script)
+    allow(script).to receive(:load).with(target_dir.join('.mamiya.script', 'script.rb')).and_return(script)
+  end
+
+  describe "#run!" do
+    it "calls hooks with proper order" do
+      hooks = %i(before_prepare prepare after_prepare)
+
+      flags = []
+      hooks.each do |sym|
+        allow(script).to receive(sym).and_return(proc { flags << sym })
+      end
+
+      expect { step.run! }.
+        to change { flags }.
+        from([]).
+        to(hooks)
+    end
+
+    it "calls after hook even if exception occured" do
+      e = Exception.new("Good bye, the cruel world")
+      allow(script).to receive(:prepare).and_return(proc { raise e })
+
+      received = nil
+      allow(script).to receive(:after_prepare).and_return(proc { |_| received = _ })
+
+      expect {
+        begin
+          step.run!
+        rescue Exception; end
+      }.
+        to change { received }.
+        from(nil).to(e)
+    end
+
+    it "calls hook in :target_dir (pwd)" do
+      pwd = nil
+      script.stub(prepare: proc { pwd = Dir.pwd })
+
+      expect {
+        step.run!
+      }.not_to change { Dir.pwd }
+
+      expect(File.realpath(pwd)).to eq target_dir.realpath.to_s
+    end
+  end
+end
