dc27ee5edb4d1354e677e4ad9164d8f30ce75967
Shota Fukumori (sora_h) <sorah@cookpad.com>
2014-09-16 13:27:03 +0900

    Add Mamiya::Agent::Tasks::Switch

---
+++ lib/mamiya/agent/tasks/switch.rb
@@ -0,0 +1,121 @@
+require 'fileutils'
+
+require 'mamiya/agent/tasks/notifyable'
+require 'mamiya/steps/switch'
+
+module Mamiya
+  class Agent
+    module Tasks
+      class Switch < Notifyable
+        class PrereleaseMissing < Exception; end
+
+        def execute
+          return unless check
+          super
+        end
+
+        def check
+          return true if prerelease_path.exist?
+          return true if release_prepared?
+
+          unless package_path.exist?
+            new_chain = ['prepare', 'switch'] + (task['_chain'] || [])
+            logger.info "Package not fetched, enqueueing fetch task with #{new_chain.inspect}"
+            task_queue.enqueue(
+              :fetch,
+              task.merge('_chain' => new_chain)
+            )
+            return false
+          end
+
+          unless prerelease_path.exist?
+            new_chain = ['switch'] + (task['_chain'] || [])
+            logger.info "Package not prepared, enqueueing prepare task with #{new_chain.inspect}"
+            task_queue.enqueue(
+              :prepare,
+              task.merge('_chain' => new_chain)
+            )
+            return false
+          end
+
+          true
+        end
+
+        def run
+          case
+          when prerelease_path.exist? && release_path.exist? && !release_path.join('.mamiya.prepared').exist?
+            logger.info "Removing existing release (not prepared)"
+            FileUtils.remove_entry_secure release_path
+          when !prerelease_path.exist? && prelease_path.exist? && !release_path.join('.mamiya.prepared').exist?
+            # this condition may be a bug
+            logger.error "Existing release is not prepared but prerelease doesn't exist"
+            raise PrereleaseMissing, "Existing release is not prepared but prerelease doesn't exist"
+          end
+
+          logger.info "Copying #{prerelease_path} -> #{release_path}"
+          FileUtils.cp_r prerelease_path, release_path
+
+          logger.info "Switching"
+          switch_step.run!
+        end
+
+        private
+
+        def application
+          task['app']
+        end
+
+        def package
+          task['pkg']
+        end
+
+        def release_name
+          task['release'] || package
+        end
+
+        def packages_dir
+          @packages_dir ||= config.packages_dir
+        end
+
+        def prereleases_dir
+          @prereleases_dir ||= config.prereleases_dir
+        end
+
+        def package_path
+          packages_dir.join(application, "#{package}.tar.gz")
+        end
+
+        def prerelease_path
+          prereleases_dir.join(application, release_name)
+        end
+
+        def releases_dir
+          config.deploy_to_for(application).join('releases').tap(&:mkpath)
+        end
+
+        def release_path
+          releases_dir.join(release_name)
+        end
+
+        def release_prepared?
+          release_path.exist? && release_path.join('.mamiya.prepared').exist?
+        end
+
+        def labels
+          @labels ||= agent.labels
+        end
+
+        def switch_step
+          @switch_step ||= Mamiya::Steps::Switch.new(
+            target: release_path,
+            config: config,
+            logger: logger,
+            config: config,
+            labels: agent.labels,
+            no_release: !!task['no_release']
+          )
+        end
+      end
+    end
+  end
+end
---
+++ spec/agent/tasks/switch_spec.rb
@@ -0,0 +1,176 @@
+require 'spec_helper'
+require 'tmpdir'
+require 'pathname'
+
+require 'mamiya/agent/tasks/switch'
+
+require 'mamiya/steps/switch'
+
+describe Mamiya::Agent::Tasks::Switch do
+  let!(:tmpdir) { Pathname.new Dir.mktmpdir("mamiya-agent-tasks-switch-spec") }
+  after { FileUtils.remove_entry_secure tmpdir }
+
+  let(:deploy_to) { tmpdir.join('deploy_to').tap(&:mkpath) }
+  let(:packages_dir) { tmpdir.join('packages').tap(&:mkpath) }
+  let(:packages_app_dir) { tmpdir.join('packages', 'myapp').tap(&:mkpath) }
+  let(:prereleases_dir) { tmpdir.join('prereleases').tap(&:mkpath) }
+  let(:prereleases_app_dir) { tmpdir.join('prereleases', 'myapp').tap(&:mkpath) }
+
+
+  let(:config) do
+    _pkg, _pre, _dto = packages_dir, prereleases_dir, deploy_to
+    Mamiya::Configuration.new.evaluate! do
+      set :packages_dir, _pkg
+      set :prereleases_dir, _pre
+      applications[:myapp] = {deploy_to: _dto}
+    end
+  end
+
+  let(:agent) { double('agent', config: config, trigger: nil, labels: [:foo, :bar]) }
+  let(:task_queue) { double('task_queue', enqueue: nil) }
+
+  let(:switch_step) { double('switch step', run!: nil) }
+
+  let(:job) { {'app' => 'myapp', 'pkg' => 'mypkg'} }
+
+  subject(:task) { described_class.new(task_queue, job, agent: agent, raise_error: true) }
+
+  it 'inherits notifyable task' do
+    expect(described_class.ancestors).to include(Mamiya::Agent::Tasks::Notifyable)
+  end
+
+  describe "#execute" do
+    context "when package not fetched and not prepared" do
+      before do
+        expect(Mamiya::Steps::Switch).not_to receive(:new)
+      end
+
+      it "enqueues fetch task and finish" do
+        expect(task_queue).to receive(:enqueue).with(
+          :fetch, job.merge('task' => 'switch', '_chain' => ['prepare', 'switch'])
+        )
+
+        task.execute
+      end
+
+      context "with _chain-ed job" do
+        let(:job) { {'app' => 'myapp', 'pkg' => 'mypkg', '_chain' => ['next']} }
+
+        it "enqueues prepare task and finish" do
+          expect(task_queue).to receive(:enqueue).with(
+            :fetch, job.merge('task' => 'switch', '_chain' => ['prepare', 'switch', 'next'])
+          )
+
+          task.execute
+        end
+      end
+    end
+
+    context "when package fetched but not prepared" do
+      before do
+        File.write packages_app_dir.join('mypkg.tar.gz'), "\n"
+        File.write packages_app_dir.join('mypkg.json'), "{}\n"
+        expect(Mamiya::Steps::Switch).not_to receive(:new)
+      end
+
+      it "enqueues prepare task and finish" do
+        expect(task_queue).to receive(:enqueue).with(
+          :prepare, job.merge('task' => 'switch', '_chain' => ['switch'])
+        )
+
+        task.execute
+      end
+
+      context "with _chain-ed job" do
+        let(:job) { {'app' => 'myapp', 'pkg' => 'mypkg', '_chain' => ['next']} }
+
+        it "enqueues prepare task and finish" do
+          expect(task_queue).to receive(:enqueue).with(
+            :prepare, job.merge('task' => 'switch', '_chain' => ['switch', 'next'])
+          )
+
+          task.execute
+        end
+      end
+    end
+
+    context "when package prepared" do
+      before do
+        File.write packages_app_dir.join('mypkg.tar.gz'), "\n"
+        File.write packages_app_dir.join('mypkg.json'), "{}\n"
+
+        prerelease = prereleases_app_dir.join('mypkg').tap(&:mkpath)
+        File.write prerelease.join('hello'), "hola\n"
+        File.write prerelease.join('.mamiya.prepared'), "#{Time.now.to_i}\n"
+
+        allow(Mamiya::Steps::Switch).to receive(:new).with(
+          target: deploy_to.join('releases', 'mypkg'),
+          labels: [:foo, :bar],
+          no_release: false,
+          config: config,
+          logger: task.logger,
+        ).and_return(switch_step)
+      end
+
+      it "copies prerelease to releases_dir" do
+        expect {
+          task.execute
+        }.to change {
+          deploy_to.join('releases', 'mypkg', 'hello').exist? &&
+          deploy_to.join('releases', 'mypkg', 'hello').read
+        }.from(false).to "hola\n"
+      end
+
+      it "calls switch step" do
+        expect(switch_step).to receive(:run!) do
+          expect(deploy_to.join('releases', 'mypkg', 'hello')).to be_exist
+        end
+        task.execute
+      end
+
+      context "when prepared release exists in releases_dir" do
+        before do
+          release = deploy_to.join('releases', 'mypkg').tap(&:mkpath)
+          File.write release.join('hehe'), ":)\n"
+          File.write release.join('.mamiya.prepared'), "#{Time.now.to_i}\n"
+        end
+
+        it "re-uses existing release" do
+          task.execute
+          expect(deploy_to.join('releases', 'mypkg', 'hehe').read).to eq ":)\n"
+        end
+      end
+
+      context "when non-prepared release exists in releases_dir" do
+        before do
+          release = deploy_to.join('releases', 'mypkg').tap(&:mkpath)
+          File.write release.join('hehe'), ":)\n"
+        end
+
+        it "re-uses existing release" do
+          task.execute
+          expect(deploy_to.join('releases', 'mypkg', 'hehe')).not_to be_exist
+          expect(deploy_to.join('releases', 'mypkg', 'hello')).to be_exist
+        end
+      end
+
+      context "with no_release" do
+        let(:job) { {'app' => 'myapp', 'pkg' => 'mypkg', 'no_release' => true} }
+
+        it "calls switch step with no_release" do
+          expect(Mamiya::Steps::Switch).to receive(:new).with(
+            target: deploy_to.join('releases', 'mypkg'),
+            labels: [:foo, :bar],
+            no_release: true,
+            config: config,
+            logger: task.logger,
+          ).and_return(switch_step)
+
+          expect(switch_step).to receive(:run!)
+
+          task.execute
+        end
+      end
+    end
+  end
+end
