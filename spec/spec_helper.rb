3422d9ae0262f06244d4cec0f4365327bb91bf1f
Kohei Suzuki <eagletmt@gmail.com>
2016-12-19 17:52:12 +0900

    Fix raise_error warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,7 +27,8 @@ unless ENV["ENABLE_LOG"]
 end
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.raise_errors_for_deprecations!
+
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
 
